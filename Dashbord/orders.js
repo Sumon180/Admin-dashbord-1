0ea761177ed3070e0a60d87dbed28f3ab56584d3
Sumon Hossain <106359888+Sumon180@users.noreply.github.com>
2022-08-26 11:42:36 +0600

    First admin dashbord

---
+++ Dashbord/orders.js
@@ -0,0 +1,40 @@
+
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '95631',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '15631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Drone with Camera drone',
+        productNumber: '25631',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '35631',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '45631',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    }
+        
+]
